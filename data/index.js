2cf113729ed8e4b3260349f755e518053dccfdf0
unknown <tyrus.miles@gmail.com>
2021-10-23 22:22:16 -0700

    added path.join so loads on all os

--- data/index.js
+++ data/index.js
@@ -1,16 +1,22 @@
 import fs from 'fs';
+import path from 'path'
 import neatCsv from 'neat-csv';
 
+// file names and paths
 const matchFile = "matches2020.csv"
 const championFile = "champion_stats.csv"
+const currentPath = process.cwd();
+
 
 export async function getMatchData(){
-    const matchData = fs.readFileSync(`./data/${matchFile}`)
+    const globalPath = path.join(currentPath, "data", matchFile)
+    const matchData = fs.readFileSync(globalPath)
     return await neatCsv(matchData)
 }
 
 export async function getChampionData(){
-    const champData = fs.readFileSync(`./data/${championFile}`);
+    const globalPath = path.join(currentPath, "data", championFile)
+    const champData = fs.readFileSync(globalPath);
     return await neatCsv(champData);
     
 }
