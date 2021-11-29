fe5bad17e228c42c66187b4e40679440716457c4
aenriq <aenriq29@asu.edu>
2021-11-28 22:44:31 -0700

    queries

---
+++ pages/roleCount.js
@@ -0,0 +1,168 @@
+import { PrismaClient } from "@prisma/client";
+import { useState, useRef, useEffect } from "react";
+
+export default function roleCount({teamDB}){
+
+    let topArr = []
+    let jungleArr = []
+    let midArr = []
+    let adcArr = []
+    let supportArr = []
+
+    let topMap = new Map()
+    let jungleMap = new Map()
+    let midMap = new Map()
+    let adcMap = new Map()
+    let supportMap = new Map()
+
+    teamDB.forEach(element => {
+        const {top, jungle, mid, adc, support} = element
+
+        topArr.push(top)
+        jungleArr.push(jungle)
+        midArr.push(mid)
+        adcArr.push(adc)
+        supportArr.push(support)
+    });
+
+    topArr.forEach(champion => {
+        if (!topMap.has(champion)) {
+            topMap.set(champion, { count: 0})
+        }
+        topMap.get(champion).count += 1
+    })
+
+    jungleArr.forEach(champion => {
+        if (!jungleMap.has(champion)) {
+            jungleMap.set(champion, { count: 0})
+        }
+        jungleMap.get(champion).count += 1
+    })
+
+    midArr.forEach(champion => {
+        if (!midMap.has(champion)) {
+            midMap.set(champion, { count: 0})
+        }
+        midMap.get(champion).count += 1
+    })
+
+    adcArr.forEach(champion => {
+        if (!adcMap.has(champion)) {
+            adcMap.set(champion, { count: 0})
+        }
+        adcMap.get(champion).count += 1
+    })
+
+    supportArr.forEach(champion => {
+        if (!supportMap.has(champion)) {
+            supportMap.set(champion, { count: 0})
+        }
+        supportMap.get(champion).count += 1
+    })
+
+    let [champList, setChampList] = useState([])
+    let category = useRef()
+
+    const getCounts = (champ, selectedMap) => {
+        const uses = selectedMap.get(champ).count
+        return {champ, uses}
+    }
+
+    // get object of stats based off champ name
+    const processChoice = (userInput) => {
+        let uniqueChamps = [...topMap.keys()]
+        let ChampRoleUse = uniqueChamps.map(champ => {
+            return getCounts(champ, topMap)
+        })
+
+        if (userInput == "Top"){
+            uniqueChamps = [...topMap.keys()]
+            ChampRoleUse = uniqueChamps.map(champ => {
+                return getCounts(champ, topMap)
+            })
+        }
+        else if (userInput == "Jungle"){
+            uniqueChamps = [...jungleMap.keys()]
+            ChampRoleUse = uniqueChamps.map(champ => {
+                return getCounts(champ, jungleMap)
+            })
+        }
+        else if (userInput == "Mid"){
+            uniqueChamps = [...midMap.keys()]
+            ChampRoleUse = uniqueChamps.map(champ => {
+                return getCounts(champ, midMap)
+            })
+        }
+        else if (userInput == "Adc"){
+            uniqueChamps = [...adcMap.keys()]
+            ChampRoleUse = uniqueChamps.map(champ => {
+                return getCounts(champ, adcMap)
+            })
+        }
+        else{
+            uniqueChamps = [...supportMap.keys()]
+            ChampRoleUse = uniqueChamps.map(champ => {
+                return getCounts(champ, supportMap)
+            })
+        }
+
+        return ChampRoleUse
+    }
+
+    const changeCat = () => {
+        const userInput = category.current.value;
+
+        let chosenRole = processChoice(userInput)
+
+        chosenRole.sort((a,b) => b.uses - a.uses);
+
+        setChampList(chosenRole)
+    }
+
+    useEffect(() => {
+        changeCat()
+    }, [])
+
+    return(
+        <div>
+            <select ref={category} onChange={() => changeCat()}>
+                <option>Top</option>
+                <option>Jungle</option>
+                <option>Mid</option>
+                <option>Adc</option>
+                <option>Support</option>
+            </select>
+            <table style={{textAlign: 'left'}}>
+                <thead style={{}}>
+                    <th style={{ width: '30ch', paddingBottom: '10px'}}>Name</th>
+                    <th style={{ paddingBottom: '10px' }}>uses</th>
+                </thead>
+                <tbody>
+                    {champList.map((champ,i) => (
+                        <tr>
+                            <td>{champ.champ}</td>
+                            <td>{champ.uses}</td>                    
+                        </tr>
+                    ))}                  
+                </tbody>                
+            </table>
+            <style jsx>{`
+                tr:nth-child(even) {   background-color: #393838; }
+            `}</style>
+        </div>
+    )
+}
+
+export async function getStaticProps(){
+
+    const prisma = new PrismaClient()
+    const teamDB = await prisma.team.findMany();
+
+    await prisma.$disconnect();
+
+    return {
+        props: {
+            teamDB
+        }
+    }
+}
---
+++ pages/sideWin.js
@@ -0,0 +1,152 @@
+import { PrismaClient } from "@prisma/client";
+import { useState, useRef } from "react";
+
+const SideWR = ({ teamDB, matchDB }) => {
+  const teamHistory = new Map();
+
+  matchDB.forEach((match) => {
+    const matchId = match.matchId;
+    let winner = match.result == 0 ? "red" : "blue";
+
+    // adding wins for champs on winning team
+    if (!teamHistory.has(match.blueTeam)) {
+      teamHistory.set(match.blueTeam, { blueW: 0, blueL: 0, redW: 0, redL: 0 });
+    }
+    if (!teamHistory.has(match.redTeam)) {
+      teamHistory.set(match.redTeam, { blueW: 0, blueL: 0, redW: 0, redL: 0 });
+    }
+
+    if (winner == "blue") {
+      teamHistory.get(match.blueTeam).blueW += 1;
+      teamHistory.get(match.redTeam).redL += 1;
+    } else {
+      teamHistory.get(match.blueTeam).blueL += 1;
+      teamHistory.get(match.redTeam).redW += 1;
+    }
+  });
+
+  // get names of all champions (the names are keys)
+  const uniqueTeams = [...teamHistory.keys()];
+
+  // get object of stats based off champ name
+  const getStats = (team) => {
+    const bluewins = teamHistory.get(team).blueW;
+    const bluelosses = teamHistory.get(team).blueL;
+    const redwins = teamHistory.get(team).redW;
+    const redlosses = teamHistory.get(team).redL;
+    const bluepercentage = Math.round(
+      (bluewins / (bluewins + bluelosses)) * 100
+    );
+    const redpercentage = Math.round((redwins / (redwins + redlosses)) * 100);
+    return {
+      team,
+      bluewins,
+      bluelosses,
+      redwins,
+      redlosses,
+      bluepercentage,
+      redpercentage,
+    };
+  };
+
+  // new array with more stats per team
+  let TeamWinPercentage = uniqueTeams.map((champ) => {
+    return getStats(champ);
+  });
+  let TeamWinRed = uniqueTeams.map((champ) => {
+    return getStats(champ);
+  });
+  // sorting for the highest win percentage (blue)
+  TeamWinPercentage.sort((a, b) => b.bluepercentage - a.bluepercentage);
+
+  TeamWinRed.sort((a, b) => b.redpercentage - a.redpercentage);
+  // state for maintaining what champs are shown
+  const [teamList, setTeamList] = useState(TeamWinPercentage);
+  const [value, setValue] = useState("blue");
+
+  const changeSort = (event) => {
+    setValue(event.target.value);
+    if (value == "blue") {
+      setTeamList(TeamWinRed);
+    } else {
+      setTeamList(TeamWinPercentage);
+    }
+  };
+  return (
+    <>
+      <select onChange={changeSort}>
+        <option selected value="blue">
+          Blue
+        </option>
+        <option value="red">Red</option>
+      </select>
+      <table style={{ textAlign: "left" }}>
+        <thead style={{}}>
+          <th style={{ width: "30ch", paddingBottom: "50px" }}>Team Name</th>
+          <th style={{ paddingBottom: "50px", paddingRight: "10px" }}>
+            Blue Wins
+          </th>
+          <th style={{ paddingBottom: "50px", paddingRight: "10px" }}>
+            Blue Losses
+          </th>
+          <th style={{ paddingBottom: "50px", paddingRight: "10px" }}>
+            Blue Percentage
+          </th>
+          <th style={{ paddingBottom: "50px", paddingRight: "10px" }}>
+            Red Wins
+          </th>
+          <th style={{ paddingBottom: "50px", paddingRight: "10px" }}>
+            Red Losses
+          </th>
+          <th style={{ paddingBottom: "50px", paddingRight: "10px" }}>
+            Red Percentage
+          </th>
+        </thead>
+        <tbody>
+          {teamList.map((team, i) => (
+            <tr>
+              <td>{team.team}</td>
+              <td>{team.bluewins}</td>
+              <td>{team.bluelosses}</td>
+              <td>{team.bluepercentage}%</td>
+              <td>{team.redwins}</td>
+              <td>{team.redlosses}</td>
+              <td>{team.redpercentage}%</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <style jsx>{`
+        tr:nth-child(even) {
+          background-color: #393838;
+        }
+        td,
+        th {
+          border: 1px solid #393838;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const prisma = new PrismaClient();
+  const teamDB = await prisma.team.findMany();
+  const matchDB = await prisma.match.findMany();
+
+  // console.log(champHistory)
+
+  // console.log(matchDB)
+
+  await prisma.$disconnect();
+
+  return {
+    props: {
+      teamDB,
+      matchDB,
+    },
+  };
+}
+
+export default SideWR;
