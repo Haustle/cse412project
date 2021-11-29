3c62fcb5f6e6dc59429fd95ce78206ab00053062
Kevin <kweinhold22@gmail.com>
2021-11-28 22:05:29 -0700

    Included Icons for the win percentage table, Centered and added margins to the UI, fixed search results bug where capital letters would fail the query.

--- pages/champWin.js
+++ pages/champWin.js
@@ -1,135 +1,152 @@
 import { PrismaClient } from "@prisma/client";
 import { useState, useRef } from "react";
+import { leagueChampPath } from "../lib/componentSnips";
 
+const Champ = ({ teamDB, matchDB }) => {
+  const champHistory = new Map();
 
-const Champ = ({teamDB, matchDB}) => {
+  matchDB.forEach((match) => {
+    const matchId = match.matchId;
+    let winner;
+    let loser;
+    winner = match.result == 0 ? match.redTeam : match.blueTeam;
+    loser = match.result == 0 ? match.blueTeam : match.redTeam;
 
+    const winComp = teamDB.filter(
+      (team) => team.matchID == matchId && team.teamName == winner
+    )[0];
+    const loseComp = teamDB.filter(
+      (team) => team.matchID == matchId && team.teamName == loser
+    )[0];
 
-    const champHistory = new Map();
+    // comps of winning and losing team
+    const winCompList = Object.values(winComp).slice(-5);
+    const loseCompList = Object.values(loseComp).slice(-5);
 
-    matchDB.forEach(match => {
-        const matchId = match.matchId;
-        let winner;
-        let loser;
-        winner = match.result == 0 ? match.redTeam : match.blueTeam;
-        loser = match.result == 0 ? match.blueTeam : match.redTeam;
+    // adding wins for champs on winning team
+    winCompList.forEach((champ) => {
+      if (!champHistory.has(champ)) {
+        champHistory.set(champ, { win: 0, lose: 0 });
+      }
+      champHistory.get(champ).win += 1;
+    });
 
-        const winComp = teamDB.filter(team => team.matchID == matchId && team.teamName == winner)[0]
-        const loseComp = teamDB.filter(team => team.matchID == matchId && team.teamName == loser)[0]
+    // add losses for champs on losing team
+    loseCompList.forEach((champ) => {
+      if (!champHistory.has(champ)) {
+        champHistory.set(champ, { win: 0, lose: 0 });
+      }
+      champHistory.get(champ).lose += 1;
+    });
+  });
 
-        // comps of winning and losing team
-        const winCompList = Object.values(winComp).slice(-5)
-        const loseCompList = Object.values(loseComp).slice(-5)
+  // get names of all champions (the names are keys)
+  const uniqueChamps = [...champHistory.keys()];
 
-        // adding wins for champs on winning team
-        winCompList.forEach((champ) => {
-            if (!champHistory.has(champ)) {
-                champHistory.set(champ, { win: 0, lose: 0 })
-            }
-            champHistory.get(champ).win += 1
-        })
+  // get object of stats based off champ name
+  const getStats = (champ) => {
+    const wins = champHistory.get(champ).win;
+    const losses = champHistory.get(champ).lose;
+    const percentage = Math.round((wins / (wins + losses)) * 100);
+    return { champ, wins, losses, percentage, total: wins + losses };
+  };
 
-        
-        // add losses for champs on losing team
-        loseCompList.forEach((champ) => {
-            if (!champHistory.has(champ)) {
-                champHistory.set(champ, { win: 0, lose: 0 })
-            }
-            champHistory.get(champ).lose += 1
-        })
-    })
+  // new array with more stats per champ
+  let ChampWinPercentage = uniqueChamps.map((champ) => {
+    return getStats(champ);
+  });
 
+  // sorting for the highest win percentage
+  ChampWinPercentage.sort((a, b) => b.percentage - a.percentage);
 
-    // get names of all champions (the names are keys)
-    const uniqueChamps = [...champHistory.keys()];
+  // state for maintaining what champs are shown
+  const [champList, setChampList] = useState(ChampWinPercentage);
+  let searchRef = useRef();
 
-    
-    // get object of stats based off champ name
-    const getStats = (champ) => {
-        const wins = champHistory.get(champ).win
-        const losses = champHistory.get(champ).lose
-        const percentage = Math.round((wins / (wins + losses)) * 100);
-        return {champ, wins, losses, percentage, total: wins + losses}
+  const sortByInput = () => {
+    const userInput = searchRef.current.value;
+    if (userInput) {
+      const filterList = ChampWinPercentage.filter((champ) =>
+        champ.champ.toLowerCase().startsWith(userInput.toLowerCase())
+      );
+      setChampList(filterList);
+    } else {
+      setChampList(ChampWinPercentage);
     }
+  };
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        marginTop: "30px",
+      }}
+    >
+      <input
+        type="search"
+        ref={searchRef}
+        onChange={() => sortByInput()}
+        style={{ marginBottom: "30px" }}
+      />
+      <table style={{ textAlign: "left" }}>
+        <thead style={{}}>
+          <th style={{ paddingBottom: "50px" }}>Name</th>
+          <th style={{ width: "30ch", paddingBottom: "50px" }}></th>
+          <th style={{ paddingBottom: "50px" }}>Wins</th>
+          <th style={{ paddingBottom: "50px" }}>Loss</th>
+          <th style={{ paddingBottom: "50px" }}>Percentage</th>
+        </thead>
+        <tbody>
+          {champList.map((champ, i) => (
+            <tr>
+              <td>
+                <img
+                  className="champ-img"
+                  src={leagueChampPath(champ.champ)}
+                  style={{
+                    width: "45px",
+                    height: "45px",
+                    borderRadius: "50%",
+                  }}
+                />
+              </td>
+              <td>{champ.champ}</td>
 
-    // new array with more stats per champ
-    let ChampWinPercentage = uniqueChamps.map(champ => {
-        return getStats(champ)
-    })
+              <td>{champ.wins}</td>
+              <td>{champ.losses}</td>
+              <td>{champ.percentage}%</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-    // sorting for the highest win percentage
-    ChampWinPercentage.sort((a,b) => b.percentage - a.percentage);
-
-
-
-    // state for maintaining what champs are shown
-    const [champList, setChampList] = useState(ChampWinPercentage)
-    let searchRef = useRef()
-
-    const sortByInput = () => {
-        const userInput = searchRef.current.value;
-        if(userInput){
-            const filterList = ChampWinPercentage.filter(champ => champ.champ.toLowerCase().startsWith(userInput));
-            setChampList(filterList)
+      <style jsx>{`
+        tr:nth-child(even) {
+          background-color: #393838;
         }
-        else{
-            setChampList(ChampWinPercentage)
-        }
-        
-    }
-    return(
-        <>
-            <input type="search" ref={searchRef} onChange={() => sortByInput()} style={{marginBottom: '30px'}}/>
-            <table style={{textAlign: 'left'}}>
-                <thead style={{}}>
-                    <th style={{ width: '30ch', paddingBottom: '50px'}}>Name</th>
-                    <th style={{ paddingBottom: '50px' }}>Wins</th>
-                    <th style={{ paddingBottom: '50px' }}>Loss</th>
-                    <th style={{ paddingBottom: '50px' }}>Percentage</th>
-                </thead>
-                <tbody>
-                    {champList.map((champ,i) => (
-                        
-                                <tr>
-                                    <td>{champ.champ}</td>
-                                    <td>{champ.wins}</td>
-                                    <td>{champ.losses}</td>
-                                    <td>{champ.percentage}%</td>
-                                </tr>
+      `}</style>
+    </div>
+  );
+};
 
-                    ))}
-                </tbody>
+export async function getStaticProps() {
+  const prisma = new PrismaClient();
+  const teamDB = await prisma.team.findMany();
+  const matchDB = await prisma.match.findMany();
 
-            </table>
-            
-            <style jsx>{`
-                tr:nth-child(even) {   background-color: #393838; }
-            `}</style>
-        </>
-    )
-}
+  // console.log(champHistory)
 
-export async function getStaticProps(){
+  // console.log(matchDB)
 
-    const prisma = new PrismaClient()
-    const teamDB = await prisma.team.findMany();
-    const matchDB = await prisma.match.findMany();
-    
-        // console.log(champHistory)
+  await prisma.$disconnect();
 
-
-    // console.log(matchDB)
-
-    await prisma.$disconnect();
-
-    return {
-        props: {
-            teamDB,
-            matchDB
-        }
-    }
-
-
+  return {
+    props: {
+      teamDB,
+      matchDB,
+    },
+  };
 }
 
 export default Champ;
