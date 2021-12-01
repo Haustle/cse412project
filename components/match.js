07f0f234c6bb5b21cbf5c8d50fac6be8e2ce8ab6
Kevin <kweinhold22@gmail.com>
2021-11-30 19:28:30 -0700

    filter matches and see winners

--- components/match.js
+++ components/match.js
@@ -1,67 +1,80 @@
-import { getTeamComp, leagueChampPath } from "../lib/componentSnips"
+import { getTeamComp, leagueChampPath } from "../lib/componentSnips";
 
+export const Match = ({ redTeam, blueTeam, matchId, table, result }) => {
+  // get the comps of redteam and blueteam
+  // returns array of {name: ChampionName, role: ChampionRole}
 
-export const Match = ({ redTeam, blueTeam, matchId, table}) => {
+  const blueComp = getTeamComp(matchId, blueTeam, table);
+  const redComp = getTeamComp(matchId, redTeam, table);
 
-    // get the comps of redteam and blueteam
-    // returns array of {name: ChampionName, role: ChampionRole}
-    
-    const blueComp = getTeamComp(matchId, blueTeam, table);
-    const redComp = getTeamComp(matchId, redTeam, table);
-
-    return(
-        <div className="match-wrapper">
-            <div className="container">
-
-                <div className="blueTeam">
-                    {blueComp.map(champ => 
-                        (<img className="champ-img" src={leagueChampPath(champ.name)}/>)
-                    )}
-                    <div style={{marginLeft: '15px'}}>{blueTeam}</div>
-                </div>
-                
-                
-                <div className="redTeam">
-                    <div style={{marginRight: '15px'}}>{redTeam}</div>
-                    
-                    {redComp.map(champ => 
-                        (<img className="champ-img" src={leagueChampPath(champ.name)} />)
-                    )}
-                </div>
-            </div>
-
-
-            <style jsx>{`
-                .container{
-                    display: flex;
-                    justify-content: space-between;
-                }
-                .match-wrapper{
-                    border: 1px solid white;
-                    width: 900px;
-                    border-radius: 10px;
-                    padding: 10px;
-                    cursor: pointer;
-                    transition: .2s all;
-                }
-
-                .match-wrapper:hover{
-                    transform: scale(1.05);
-                }
-                .champ-img{
-                    border-radius: 50%;
-                    height: 40px;
-                    width: 40px;
-                }
-                .redTeam, .blueTeam{
-                    display: flex;
-                    align-items: center;
-                }
-                .redTeam {
-                    margin-left: 20px;
-                    just
-                }
-            `}</style>
+  return (
+    <div className="match-wrapper">
+      <div className="container">
+        <div className="blueTeam">
+          {blueComp.map((champ) => (
+            <img className="champ-img" src={leagueChampPath(champ.name)} />
+          ))}
+          <div
+            style={{
+              marginLeft: "15px",
+              color: result == 1 ? "gold" : "white",
+            }}
+          >
+            {blueTeam}
+          </div>
         </div>
-    )
-}
+
+        <div className="redTeam">
+          <div
+            style={{
+              marginRight: "15px",
+              color: result == 0 ? "gold" : "white",
+            }}
+          >
+            {redTeam}
+          </div>
+
+          {redComp.map((champ) => (
+            <img className="champ-img" src={leagueChampPath(champ.name)} />
+          ))}
+        </div>
+      </div>
+
+      <style jsx>{`
+        .container {
+          display: flex;
+          justify-content: space-between;
+        }
+        .match-wrapper {
+          border: 1px solid white;
+          width: 900px;
+          border-radius: 10px;
+          padding: 10px;
+          cursor: pointer;
+          transition: 0.2s all;
+        }
+
+        .match-wrapper:hover {
+          transform: scale(1.05);
+        }
+        .champ-img {
+          border-radius: 50%;
+          height: 40px;
+          width: 40px;
+        }
+        .redTeam,
+        .blueTeam {
+          display: flex;
+          align-items: center;
+        }
+        .redTeam {
+          margin-left: 20px;
+          border: 1px solid red;
+        }
+        .blueTeam {
+          border: 1px solid blue;
+        }
+      `}</style>
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,78 +1,102 @@
 // import { getMatchData } from '../data/index';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 // import { createTeam, createMatch, createChampions } from '../lib/parseMatchData';
+import { useState, useRef } from "react";
+import { Match } from "../components/match";
 
-import { Match } from '../components/match';
+export default function Index({ teams, matches }) {
+  let teamSet = new Set();
+  matches.forEach((element) => {
+    const redTeam = element.redTeam;
+    const blueTeam = element.blueTeam;
 
+    teamSet.add(redTeam);
+    teamSet.add(blueTeam);
+  });
+  // matches = matches.slice(1, 50);
+  // Remove all items from set and place inside of array
+  const teamArray = [...teamSet];
+  let teamRef = useRef();
+  const [teamName, setTeamName] = useState("All");
 
-export default function Index({teams, matches}) {
+  const onTeamUpdate = () => {
+    const selectedTeam = teamRef.current.value;
+    setTeamName(selectedTeam);
+  };
 
-  
-    matches = matches.slice(1,50)
-    return (
-      <div className="wrapper">
-        <div className="container">
-          {matches.map((match, i) => (
+  console.log(teamArray);
+
+  return (
+    <div className="wrapper">
+      <div className="container">
+        <select onChange={() => onTeamUpdate()} ref={teamRef}>
+          <option>All</option>
+          {teamArray.map((team, i) => (
+            <option>{team}</option>
+          ))}
+        </select>
+        {matches.map((match, i) =>
+          match.blueTeam == teamName ||
+          match.redTeam == teamName ||
+          teamName == "All" ? (
             <div className="mb-50" key={match.matchId}>
               <Match
                 redTeam={match.redTeam}
                 blueTeam={match.blueTeam}
                 matchId={match.matchId}
-                table={teams} />
-
+                table={teams}
+                result={match.result}
+              />
             </div>
-          ))}
-        </div>
-        
-        
-        <style jsx>{`
-          .container{
-
-          }
-          .wrapper{
-            display: flex;
-            justify-content:center;
-            margin-top: 100px;
-          }
-          .mr-20{
-            margin-right: 20px;
-          }
-
-          .mb-50{
-            margin-bottom: 25px;
-          }
-        `}</style>
+          ) : null
+        )}
       </div>
-    )
+
+      <style jsx>{`
+        .container {
+        }
+        .wrapper {
+          display: flex;
+          justify-content: center;
+          margin-top: 100px;
+        }
+        .mr-20 {
+          margin-right: 20px;
+        }
+
+        .mb-50 {
+          margin-bottom: 25px;
+        }
+      `}</style>
+    </div>
+  );
 }
 
-export async function getStaticProps(){
-
+export async function getStaticProps() {
   // making prisma connection
-  const prisma = new PrismaClient()
+  const prisma = new PrismaClient();
 
   // dont delete this, think it may break program for some reason
   // const matchData = await getMatchData();
 
-
   // check size on team Table
   const teamDB = await prisma.team.findMany();
-  if(teamDB.length == 0){
-    console.log('adding teams to Teams table...')
-    createTeam()
+  if (teamDB.length == 0) {
+    console.log("adding teams to Teams table...");
+    createTeam();
   }
 
   // check size on match db
   const matchDB = await prisma.match.findMany();
-  if(matchDB.length == 0){
-    console.log('adding matches to Match table...')
-    createMatch()
+  if (matchDB.length == 0) {
+    console.log("adding matches to Match table...");
+    createMatch();
   }
   // console.log(matchDB)
   // check size on Champions table
   const champDB = await prisma.champion.findMany();
-  if(champDB.length == 0){
-    console.log('adding champions to table....')
+  if (champDB.length == 0) {
+    console.log("adding champions to table....");
     createChampions();
   }
 
@@ -81,13 +105,13 @@ export async function getStaticProps(){
   // await prisma.match.deleteMany({})
 
   // close the prisma connection
-  await prisma.$disconnect()
+  await prisma.$disconnect();
 
   return {
     props: {
       // champs: champDB,
       matches: matchDB,
-      teams: teamDB
-    }
-  }
+      teams: teamDB,
+    },
+  };
 }
